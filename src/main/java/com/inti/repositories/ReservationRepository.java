ed2e0b8a793effeb876dbb214bac6144f105e3b7
Angélique <angel.belg@hotmail.fr>
2023-05-25 17:52:54 +0200

    maj class

---
+++ src/main/java/com/inti/repositories/ReservationRepository.java
@@ -0,0 +1,13 @@
+package com.inti.repositories;
+
+import java.util.Date;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.inti.entities.Reservation;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Long>{
+	Reservation findByDate(Date dateResa);
+}
---
+++ src/main/java/com/inti/services/impl/ReservationService.java
@@ -0,0 +1,43 @@
+package com.inti.services.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.inti.entities.Reservation;
+import com.inti.services.interfaces.IReservationService;
+
+@Service
+public class ReservationService implements IReservationService{
+
+	@Override
+	public List<Reservation> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Reservation findOne(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Reservation save(Reservation reservation) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Reservation findById(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
