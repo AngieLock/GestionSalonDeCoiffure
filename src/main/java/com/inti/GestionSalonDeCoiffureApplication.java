100d93cabe69db05a678098b66c313a08d4dadf8
Angélique <angel.belg@hotmail.fr>
2023-05-25 15:43:09 +0200

    création du projet

---
+++ src/main/java/com/inti/GestionSalonDeCoiffureApplication.java
@@ -0,0 +1,13 @@
+package com.inti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionSalonDeCoiffureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionSalonDeCoiffureApplication.class, args);
+	}
+
+}
