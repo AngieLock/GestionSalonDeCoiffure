346b9eade7d1e4555e147304e57798d697b64bee
margsalanne <margaux.salanne@gmail.com>
2023-05-25 17:14:39 +0200

    commit 1

---
+++ src/main/java/com/inti/entities/Utilisateur.java
@@ -0,0 +1,122 @@
+
+package com.inti.entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.JoinColumn;
+
+@Entity
+@Table(name = "UTILISATEURS", schema = "gestion_coiffure_db")
+public class Utilisateur implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idUtilisateur;
+	@Column(name = "nom_utilisateur")
+	private String nomUtilisateur;
+	private String prenomUtilisateur;
+	// Les attributs pour la sécurité :
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "PROFILS", joinColumns = @JoinColumn(name = "id_user", referencedColumnName = "idUtilisateur"), inverseJoinColumns = @JoinColumn(name = "id_role", referencedColumnName = "idRole"))
+	private Set<Role> roles = new HashSet<>();
+	@Column(unique = true)
+	private String login;
+	private String password;
+	private String dateNaissance;
+	private boolean enabled = true;
+	@OneToMany(mappedBy="utilisateur")
+	private List<Avis> avis=new ArrayList<>();
+	
+	public Utilisateur() {
+	}
+	
+	public Utilisateur(String nomUtilisateur, String prenomUtilisateur, Set<Role> roles, String login, String password,
+			String dateNaissance, boolean enabled, List<Avis> avis) {
+		this.nomUtilisateur = nomUtilisateur;
+		this.prenomUtilisateur = prenomUtilisateur;
+		this.roles = roles;
+		this.login = login;
+		this.password = password;
+		this.dateNaissance = dateNaissance;
+		this.enabled = enabled;
+		this.avis = avis;
+	}
+
+	public Long getIdUtilisateur() {
+		return idUtilisateur;
+	}
+	public void setIdUtilisateur(Long idUtilisateur) {
+		this.idUtilisateur = idUtilisateur;
+	}
+	public String getNomUtilisateur() {
+		return nomUtilisateur;
+	}
+	public void setNomUtilisateur(String nomUtilisateur) {
+		this.nomUtilisateur = nomUtilisateur;
+	}
+	public String getPrenomUtilisateur() {
+		return prenomUtilisateur;
+	}
+	public void setPrenomUtilisateur(String prenomUtilisateur) {
+		this.prenomUtilisateur = prenomUtilisateur;
+	}
+	public Set<Role> getRoles() {
+		return roles;
+	}
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getDateNaissance() {
+		return dateNaissance;
+	}
+	public void setDateNaissance(String dateNaissance) {
+		this.dateNaissance = dateNaissance;
+	}
+	public boolean isEnabled() {
+		return enabled;
+	}
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+	public List<Avis> getAvis() {
+		return avis;
+	}
+
+	public void setAvis(List<Avis> avis) {
+		this.avis = avis;
+	}
+	@Override
+	public String toString() {
+		return "Utilisateur [idUtilisateur=" + idUtilisateur + ", nomUtilisateur=" + nomUtilisateur
+				+ ", prenomUtilisateur=" + prenomUtilisateur + ", roles=" + roles + ", login=" + login + ", password="
+				+ password + ", dateNaissance=" + dateNaissance + ", enabled=" + enabled + "]";
+	}
+	
+	
+
+}
